275c5d2a23144b60c6562a0d716d0b63a48d824c
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-06-20 14:59:36 +0200

    Fix and test model associations for User, Event and Attendance

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,6 @@
 class Event < ApplicationRecord
-  validates :name, presence: true
-  validates :category, presence: true
-  validates :location, presence: true
-  validates :event_time, presence: true
-  validates :description, presence: true
+
 
   belongs_to :user
+  has_many :attendances, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :events, dependent: :destroy
+  has_many :attendances, dependent: :destroy
 end
