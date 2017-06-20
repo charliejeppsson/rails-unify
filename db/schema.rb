07b2af0d3f9825ff6e9b736bf0e3b0dbeff8926f
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-06-20 23:35:07 +0200

    Setup rails admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620145608) do
+ActiveRecord::Schema.define(version: 20170620210414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20170620145608) do
     t.string "industry"
     t.string "location"
     t.datetime "date_of_birth"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
