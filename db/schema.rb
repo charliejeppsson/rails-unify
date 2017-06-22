4fd61eec34a7143faa10fcb5773aed1d35992910
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-06-22 22:57:13 +0200

    Fix profile and attendants list on event#show

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170622092511) do
+ActiveRecord::Schema.define(version: 20170622141521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +89,7 @@ ActiveRecord::Schema.define(version: 20170622092511) do
     t.datetime "token_expiry"
     t.string "picture"
     t.string "url"
+    t.boolean "is_public", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
