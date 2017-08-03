e9e5bde14f62621111261cdb6428b0a8541cfbd4
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-08-03 12:10:43 +0200

    fix typo in datetimepicker.js

--- app/assets/javascripts/datetimepicker.js
+++ app/assets/javascripts/datetimepicker.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
-  $('#datetimepicker1').datetimepicker(format: 'yyyy-mm-dd hh:ii'});
-  $('#datetimepicker2').datetimepicker(format: 'yyyy-mm-dd hh:ii'});
+  $('#datetimepicker1').datetimepicker();
+  $('#datetimepicker2').datetimepicker();
 
   $("#datetimepicker1").on("dp.change", function (e) {
   $('#datetimepicker2').data("DateTimePicker").minDate(e.date);
